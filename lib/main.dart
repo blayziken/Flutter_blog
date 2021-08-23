ef96ee9b073055634fd80f0dd781b17699276eac
blayziken <kingtoluwalope@gmail.com>
2021-08-23 17:10:34 +0100

    cleared TODO's

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     checkLogin();
   }
--- lib/screens/Profile/MainProfile.dart
+++ lib/screens/Profile/MainProfile.dart
@@ -16,7 +16,6 @@ class _MainProfileScreenState extends State<MainProfileScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     fetchProfileData();
   }
