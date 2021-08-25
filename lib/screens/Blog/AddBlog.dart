0ea37d4c82b9c8229822a2e4c4f9d43c7dd3b65f
blayziken <kingtoluwalope@gmail.com>
2021-08-25 13:27:54 +0100

    added spinner on submit

--- lib/screens/Blog/AddBlog.dart
+++ lib/screens/Blog/AddBlog.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:blog_app/screens/Home/Home.dart';
 import 'package:blog_app/services/NetworkHandler.dart';
 import 'package:blog_app/utils/marginUtils.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,8 @@ class AddBlogScreen extends StatefulWidget {
 }
 
 class _AddBlogScreenState extends State<AddBlogScreen> {
+  bool _spinner = false;
+
   // Image Picker
   PickedFile _imageFile;
   final ImagePicker _picker = ImagePicker();
@@ -34,6 +37,9 @@ class _AddBlogScreenState extends State<AddBlogScreen> {
 
   // API Network Handler Call
   NetworkHandler networkHandler = NetworkHandler();
+
+  // For SnackBar
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   Widget titleTextField() {
     return Padding(
@@ -104,47 +110,93 @@ class _AddBlogScreenState extends State<AddBlogScreen> {
   }
 
   Widget addButton() {
-    return Center(
-      child: InkWell(
-        child: Container(
-          height: 60,
-          width: 150,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.teal),
-          child: Center(
-            child: Text(
-              'Add Post',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+    return _spinner
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Center(
+            child: InkWell(
+              child: Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.teal),
+                child: Center(
+                  child: Text(
+                    'Add Post',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
+              onTap: () async {
+                setState(() {
+                  _spinner = true;
+                });
+
+                if (_imageFile == null) {
+                  final snackBar4 = SnackBar(
+                    content: Text('Cover Image Needed'),
+                    backgroundColor: Colors.red,
+                  );
+                  return _scaffoldKey.currentState.showSnackBar(snackBar4);
+                }
+
+                if (_imageFile != null && _formKey.currentState.validate()) {
+                  Map<String, String> body = {
+                    "title": _titleController.text,
+                    "body": _bodyController.text,
+                  };
+
+                  // POST RESPONSE
+                  var response = await networkHandler.postData('posts/add', body);
+
+                  if (response.statusCode == 200 || response.statusCode == 201) {
+                    // GETTING THIS POST ID FROM THE RESPONSE BODY SO I CAN PATCH THE COVER IMAGE
+                    var postId = json.decode(response.body)["data"]["postId"];
+
+                    var imageResponse = await networkHandler.patchImage('posts/$postId/addCoverImage', _imageFile.path);
+
+                    if (imageResponse.statusCode == 200 || response.statusCode == 201) {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(),
+                          ),
+                          (route) => false);
+                    }
+                    if (imageResponse.statusCode == 404) {
+                      setState(() {
+                        _spinner = false;
+                      });
+
+                      final snackBar5 = SnackBar(
+                        content: Text('Something went wrong...Try again'),
+                        backgroundColor: Colors.red,
+                      );
+                      return _scaffoldKey.currentState.showSnackBar(snackBar5);
+                    }
+                  }
+
+                  if (response.statusCode == 404) {
+                    setState(() {
+                      _spinner = false;
+                    });
+
+                    final snackBar3 = SnackBar(
+                      content: Text('Something went wrong...Try again'),
+                      backgroundColor: Colors.red,
+                    );
+                    return _scaffoldKey.currentState.showSnackBar(snackBar3);
+                  }
+                } else {
+                  print('Error: Add Blog Button - Blog not added!!!');
+                }
+              },
             ),
-          ),
-        ),
-        onTap: () async {
-          if (_imageFile != null && _formKey.currentState.validate()) {
-            Map<String, String> body = {
-              "title": _titleController.text,
-              "body": _bodyController.text,
-            };
-
-            // POST RESPONSE
-            var response = await networkHandler.postData('posts/add', body);
-
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // GETTING THIS POST ID FROM THE RESPONSE BODY SO I CAN PATCH THE COVER IMAGE
-              var postId = json.decode(response.body)["data"]["postId"];
-
-              var imageResponse = await networkHandler.patchImage('posts/$postId/addCoverImage', _imageFile.path);
-
-              print(imageResponse.statusCode);
-            }
-          } else {
-            print('Error: Add Blog Button - Blog not added!!!');
-          }
-        },
-      ),
-    );
+          );
   }
 
   @override
