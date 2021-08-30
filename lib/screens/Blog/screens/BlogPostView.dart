054258a7aa3e38602ae3eebe501973d83e826799
blayziken <kingtoluwalope@gmail.com>
2021-08-30 16:50:13 +0100

    fixed minor UI issues

--- lib/screens/Blog/screens/BlogPostView.dart
+++ lib/screens/Blog/screens/BlogPostView.dart
@@ -31,31 +31,37 @@ class FullPostView extends StatelessWidget {
                 ),
               ),
 //            customYMargin(5),
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      addBlogModel.title,
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 18.0, top: 0),
+                    child: Card(
+                      child: Container(
+                        width: double.infinity,
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            addBlogModel.title,
+                            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+                          ),
+                        ),
+                      ),
                     ),
-                    customYMargin(20),
-                    Text(
+                  ),
+//                  customYMargin(20),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0, top: 15.0),
+                    child: Text(
                       addBlogModel.body,
                       style: TextStyle(
                         fontSize: 20,
-                        letterSpacing: 1,
-                        wordSpacing: 3.0,
+//                        letterSpacing: 1,
+                        wordSpacing: 2.0,
                       ),
                     ),
-//                  Container(
-//                    color: Colors.red,
-//                    width: double.infinity,
-//                    height: 200,
-//                  ),
-                  ],
-                ),
+                  ),
+                ],
               ),
 //            Spacer(),
               customYMargin(30),
@@ -66,15 +72,15 @@ class FullPostView extends StatelessWidget {
                   children: [
                     Text(
                       'Likes: ',
-                      style: TextStyle(fontSize: 18),
+                      style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
                     ),
                     Text(
                       'Comments: ',
-                      style: TextStyle(fontSize: 18),
+                      style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
                     ),
                     Text(
                       'Shares: ',
-                      style: TextStyle(fontSize: 18),
+                      style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
                     ),
                   ],
                 ),
